1a266e28d3543571b777f39dbb272c7b7f940c34
Ian Bull <irbull@eclipsesource.com>
2015-07-03 14:15:16 -0700

    .

--- src/android/Echo.java
+++ src/android/Echo.java
@@ -8,16 +8,18 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import com.eclipsesource.v8.V8;
+import android.view.View;
+import android.widget.Switch;
+
 import com.eclipsesource.tabris.android.TabrisActivity;
-
-import com.eclipsesource.tabris.android.internal.toolkit.view.Swipe;
-import com.eclipsesource.tabris.android.internal.toolkit.view.SwipeAdapter;
+import com.eclipsesource.tabris.android.internal.toolkit.property.IPropertyHandler;
+import com.eclipsesource.tabris.android.internal.toolkit.property.SwitchPropertyHandler;
+import com.eclipsesource.tabris.client.core.RemoteObject;
 import com.eclipsesource.tabris.client.core.model.Properties;
-import com.eclipsesource.tabris.client.core.operation.CallOperation;
 import com.eclipsesource.tabris.client.core.operation.CreateOperation;
-import com.eclipsesource.tabris.client.core.operation.SetOperation;
+import com.eclipsesource.tabris.client.core.operation.ListenOperation;
 
-import static com.eclipsesource.tabris.client.core.ProtocolConstants.*;
+import static com.eclipsesource.tabris.client.core.ProtocolConstants.EVENT_CHECKED;
 import static com.eclipsesource.tabris.client.core.util.ValidationUtil.*;
 
 import com.eclipsesource.tabris.android.internal.toolkit.operator.AbstractAndroidOperator;
@@ -29,87 +31,71 @@ import com.eclipsesource.tabris.client.core.OperatorRegistry;
  */
 public class Echo extends CordovaPlugin {
 
+  public static class SwitchOperator2 extends ButtonOperator {
 
-  public static class SwipeOperator2 extends AbstractAndroidOperator {
+    public static final String TYPE = "tabris.Switch2";
 
-   public static final String TYPE = "tabris.Swipe2";
+    private final SwitchPropertyHandler handler;
 
-   public SwipeOperator2( TabrisActivity activity ) {
-     super( activity );
-   }
+    public SwitchOperator( TabrisActivity activity ) {
+      super( activity );
+      handler = new SwitchPropertyHandler( activity );
+    }
 
-   @Override
-   public String getType() {
-     return TYPE;
-   }
+    @Override
+    public String getType() {
+      return TYPE;
+    }
 
-   @Override
-   public void create( CreateOperation operation ) {
-     validateCreateOperation( operation );
-     String parentId = operation.getProperties().getString( PROP_PARENT );
-     Swipe swipe = findObjectById( parentId, Swipe.class );
-     SwipeAdapter swipeAdapter = new SwipeAdapter( getActivity(), swipe );
-     swipe.setAdapter( swipeAdapter );
-     getObjectRegistry().register( operation.getTarget(), swipeAdapter, operation.getType() );
-     getActivity().getRemoteObject( swipeAdapter ).addListen( EVENT_SWIPE );
-     setItemCount( swipeAdapter, operation.getProperties() );
-   }
+    @Override
+    protected IPropertyHandler getPropertyHandler( Object object ) {
+      return handler;
+    }
 
-   @Override
-   public Object call( CallOperation operation ) {
-     validateCallOperation( operation );
-     SwipeAdapter swipeAdapter = findObjectById( operation.getTarget(), SwipeAdapter.class );
-     Properties properties = operation.getProperties();
-     if( hasMethod( operation, PROP_ADD ) ) {
-       swipeAdapter.addItemAt( properties.getInteger( PROP_INDEX ),
-           properties.getString( PROP_CONTROL ) );
-     } else if( hasMethod( operation, PROP_REMOVE ) ) {
-       swipeAdapter.removeItemsAt( properties.getList( PROP_ITEMS, Integer.class ) );
-     } else if( hasMethod( operation, PROP_LOCK_LEFT ) ) {
-       swipeAdapter.lockLeft( properties.getInteger( PROP_INDEX ) );
-     } else if( hasMethod( operation, PROP_LOCK_RIGHT ) ) {
-       swipeAdapter.lockRight( properties.getInteger( PROP_INDEX ) );
-     } else if( hasMethod( operation, PROP_UNLOCK_LEFT ) ) {
-       swipeAdapter.lockLeft( SwipeAdapter.UNLOCKED );
-     } else if( hasMethod( operation, PROP_UNLOCK_RIGHT ) ) {
-       swipeAdapter.lockRight( SwipeAdapter.UNLOCKED );
-     }
-     return null;
-   }
+    @Override
+    public void create( CreateOperation operation ) {
+      validateCreateOperation( operation );
+      initiateNewView( operation, new Switch( getActivity() ) );
+    }
 
-   private boolean hasMethod( CallOperation operation, String method ) {
-     return operation.getMethod().equals( method );
-   }
+    @Override
+    public void listen( ListenOperation operation ) {
+      super.listen( operation );
+      validateListenOperation( operation );
+      Properties properties = operation.getProperties();
+      if( properties.hasProperty( EVENT_CHECKED ) ) {
+        View view = findViewByTarget( operation );
+        if( properties.getBoolean( EVENT_CHECKED ) ) {
+          view.setOnClickListener( new SwitchClickListener( getActivity() ) );
+        } else {
+          view.setOnClickListener( null );
+        }
+      }
+    }
 
-   @Override
-   public void set( SetOperation operation ) {
-     SwipeAdapter swipeAdapter = findObjectById( operation.getTarget(), SwipeAdapter.class );
-     Properties properties = operation.getProperties();
-     setItemCount( swipeAdapter, properties );
-     setActiveItem( swipeAdapter, properties );
-   }
+    static class SwitchClickListener implements View.OnClickListener {
 
-   private void setItemCount( SwipeAdapter swipeAdapter, Properties properties ) {
-     Integer itemCount = properties.getInteger( PROP_ITEM_COUNT );
-     if( itemCount != null ) {
-       swipeAdapter.setItemCount( itemCount );
-       swipeAdapter.notifyDataSetChanged();
-     }
-   }
+      private final TabrisActivity activity;
 
-   private void setActiveItem( SwipeAdapter swipeAdapter, Properties properties ) {
-     Integer active = properties.getInteger( PROP_ACTIVE );
-     if( active != null ) {
-       swipeAdapter.setActiveItem( active );
-     }
-   }
- }
+      public SwitchClickListener( TabrisActivity activity ) {
+        this.activity = activity;
+      }
+
+      @Override
+      public void onClick( View view ) {
+        RemoteObject remoteObject = activity.getRemoteObject( view );
+        remoteObject.notify( EVENT_CHECKED, EVENT_CHECKED, ( ( Switch )view ).isChecked() );
+      }
+
+    }
+
+  }
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         if (action.equals("echo")) {
             TabrisActivity activity = (TabrisActivity) cordova.getActivity();
-            SwipeOperator2 operator = new  SwipeOperator2(activity);
+            SwitchOperator2 operator = new  SwitchOperator2(activity);
             OperatorRegistry operatorRegistry = activity.getWidgetToolkit().getOperatorRegistry();
             operatorRegistry.register( operator.getType(), operator );
             this.echo(operator.getType(), callbackContext);
